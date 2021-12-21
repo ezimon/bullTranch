88bcc500eb9ad0f99c073cef9819c0366677f4d9
Simon <simonespeche123@gmail.com>
2021-12-21 14:50:59 -0300

    css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BigMenu } from "./BigMenu";
-import apiBG from "../assets/bigMenu/homeBG.jpg";
+import apiBG from "../assets/bigMenu/liveBG.jpg";
 
 export const Home = ({ setRoute, route }) => {
   return (
     <div className="noscroll">
-        <img src={apiBG} className="bgImg homeBG" />
+        <img src={apiBG} className="bgImg" />
         <div className="component tc">
           <BigMenu setRoute={setRoute} route={route} />
         </div>
