5ba5ea73c797d6f6405e12e12b6b5b6bdfaefba6
Simon <simonespeche123@gmail.com>
2022-01-04 15:01:46 -0300

    final mobile wouches

--- src/components/SPnavBarr.js
+++ src/components/SPnavBarr.js
@@ -7,55 +7,95 @@ import hydroIcon from "../assets/icons/hydro.png";
 export const SPnavBarr = ({ setInd, prog, ind }) => {
   return (
     <div className="hoMenuSmall">
-      <div className="progContsmall">
-        <div
-          className="progBar"
-          style={{
-            width: `${JSON.stringify(prog)}%`,
-          }}
-        ></div>
-      </div>
       <div className="navbarCont">
-        <img
-          className={
-            ind === 0
-              ? "slctds navbarIcon grow pointer"
-              : "navbarIcon grow pointer"
-          }
-          src={liveIcon}
-          alt=""
-          onClick={() => setInd(0)}
-        />
-        <img
-          className={
-            ind === 1
-              ? "slctds navbarIcon grow pointer"
-              : "navbarIcon grow pointer"
-          }
-          src={agriIcon}
-          alt=""
-          onClick={() => setInd(1)}
-        />
-        <img
-          className={
-            ind === 2
-              ? "slctds navbarIcon grow pointer"
-              : "navbarIcon grow pointer"
-          }
-          src={apiIcon}
-          alt=""
-          onClick={() => setInd(2)}
-        />
-        <img
-          className={
-            ind === 3 
-              ? "slctds navbarIcon grow pointer"
-              : "navbarIcon grow pointer"
-          }
-          src={hydroIcon}
-          alt=""
-          onClick={() => setInd(3)}
-        />
+        <div>
+          {ind === 0 ? (
+            <div className="progContsmall">
+              <div
+                className="progBar"
+                style={{
+                  width: `${JSON.stringify(prog)}%`,
+                }}
+              ></div>
+            </div>
+          ) : null}
+          <img
+            className={
+              ind === 0
+                ? "slctds navbarIcon grow pointer"
+                : "navbarIcon grow pointer"
+            }
+            src={liveIcon}
+            alt=""
+            onClick={() => setInd(0)}
+          />
+        </div>
+        <div>
+          {ind === 1 ? (
+            <div className="progContsmall">
+              <div
+                className="progBar"
+                style={{
+                  width: `${JSON.stringify(prog)}%`,
+                }}
+              ></div>
+            </div>
+          ) : null}
+          <img
+            className={
+              ind === 1
+                ? "slctds navbarIcon grow pointer"
+                : "navbarIcon grow pointer"
+            }
+            src={agriIcon}
+            alt=""
+            onClick={() => setInd(1)}
+          />
+        </div>
+        <div>
+          {ind === 2 ? (
+            <div className="progContsmall">
+              <div
+                className="progBar"
+                style={{
+                  width: `${JSON.stringify(prog)}%`,
+                }}
+              ></div>
+            </div>
+          ) : null}
+          <img
+            className={
+              ind === 2
+                ? "slctds navbarIcon grow pointer"
+                : "navbarIcon grow pointer"
+            }
+            src={apiIcon}
+            alt=""
+            onClick={() => setInd(2)}
+          />
+        </div>
+        <div>
+          {ind === 3 ? (
+            <div className="progContsmall">
+              <div
+                className="progBar"
+                style={{
+                  width: `${JSON.stringify(prog)}%`,
+                }}
+              ></div>
+            </div>
+          ) : null}
+          <img
+            className={
+              ind === 3
+                ? "slctds navbarIcon grow pointer"
+                : "navbarIcon grow pointer"
+            }
+            src={hydroIcon}
+            alt=""
+            onClick={() => setInd(3)}
+          />
+        </div>
       </div>
     </div>
   );
